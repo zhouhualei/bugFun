86317b373f700cb4202eaf6da9aa908692276132
罗旭 <luox199111@gmail.com>
2015-06-18 23:57:34 +0800

    add some function

--- js/app.js
+++ js/app.js
@@ -4,5 +4,27 @@ $(function(){
 	$("#submitby").selectpicker();
 	$("#owner").selectpicker();
 	
+	 $("#project").popover({
+            trigger:'manual',
+            placement : 'bottom', //placement of the popover. also can use top, bottom, left or right
+            title : 'Project', //this is the top title bar of the popover. add some basic css
+            html: 'true', //needed to show html of course
+            content : 
+            '<span>cepm</span><br/><span>ccsidm</span><br/><span>ican</span>', //this is the content of the html box. add the image here or anything you want really.
+            animation: false
+        }).on("mouseenter", function () {
+                    var _this = this;
+                    $(this).popover("show");
+                    $(this).siblings(".popover").on("mouseleave", function () {
+                        $(_this).popover('hide');
+                    });
+                }).on("mouseleave", function () {
+                    var _this = this;
+                    setTimeout(function () {
+                        if (!$(".popover:hover").length) {
+                            $(_this).popover("hide")
+                        }
+                    }, 100);
+                });
 });
 
